aadd46c69a0d170e68e8047409c0b6ca9bb7f924
Joe Lamb <joe@elevenplustwo.co.uk>
2018-10-02 21:42:11 +0100

    Google map set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import CrimeMap from './CrimeMap'
 
 import '../styles/components/app.scss';
 
 class App extends React.Component {
 
-  constructor(){
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  render(){
-    return (
-      <div className="app">
-        App goes here
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="app">
+                <CrimeMap
+                    isMarkerShown
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7E9Z1JUBNM-AuoGIeK-YXGjAUz4yG2i0&v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/CrimeMap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+
+const CrimeMap = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 51.4111, lng: - 0.050184 }}
+    >
+        {props.isMarkerShown && <Marker position={{ lat: 51.4111, lng: - 0.050184 }} />}
+    </GoogleMap>
+))
+
+export default CrimeMap;
