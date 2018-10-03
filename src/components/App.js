2654ca07015b746717def0f087050e8cefedc41d
Joe Lamb <joe@elevenplustwo.co.uk>
2018-10-03 16:58:08 +0100

    advance round with win or lose result

--- src/components/App.js
+++ src/components/App.js
@@ -109,7 +109,7 @@ class App extends React.Component {
         }
         this.setState({
             roundResult: outcome
-        })
+        }, () => this.state.roundResult !== 'draw' && this.advanceRound())
     }
 
     advanceRound() {
