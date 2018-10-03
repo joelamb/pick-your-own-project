86f165b9377287373ffd6e3aa20a4c6047e8f9bc
Joe Lamb <joe@elevenplustwo.co.uk>
2018-10-03 15:28:08 +0100

    basic card styling for mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Property from './Property';
 
+import '../styles/components/card.scss';
+
 const Card = ({ title, properties }) => {
     return (
         <div className="card">
             <h2 className="card__title">{title}</h2>
-            <img src="https://via.placeholder.com/350x150" alt="" />
-            <ul className="properties">
+            <img className="card__image" src="https://via.placeholder.com/350x150" alt="" />
+            <ul className="card__properties">
                 {Object.keys(properties).map((key, i) => {
                     return <Property key={i} property={key} value={properties[key]} />
                 })}
--- src/components/Property.js
+++ src/components/Property.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../styles/components/property.scss';
+
 const Property = ({ property, value }) => {
     return (
         <li className="card__property">{property}<span>{value}</span></li>
