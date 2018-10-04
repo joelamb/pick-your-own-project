067aa05c45f0d6d478da463a3df15aa205a70a64
Joe Lamb <joe@elevenplustwo.co.uk>
2018-10-04 08:52:02 +0100

    card deal test (incomplete)

--- tests/components/App.test.js
+++ tests/components/App.test.js
@@ -3,8 +3,16 @@ import App from '../../src/components/App';
 import renderer from 'react-test-renderer';
 
 describe('App', () => {
-  test('matches the snapshot', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+    //   test('matches the snapshot', () => {
+    //     const tree = renderer.create(<App />).toJSON();
+    //     expect(tree).toMatchSnapshot();
+    //   });
+
+    test('deal cards', () => {
+        const expected = 3;
+        const result = this.dealCards([])
+
+    }
+
+    )
 });
