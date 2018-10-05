08264dd5b9a1a51782024c15fc40d4e7ae79d56c
Joe Lamb <joe@elevenplustwo.co.uk>
2018-10-05 11:19:00 +0100

    start page styling

--- src/components/Start.js
+++ src/components/Start.js
@@ -8,31 +8,44 @@ class Start extends React.Component {
     this.startTimer = this.startTimer.bind(this);
   }
 
-  startTimer() {
-
-  }
+  startTimer() {}
   render() {
     return (
-      <div className='start'>
-        <h1 className='start__title'>Play Your Cards Right</h1>
+      <div className="start">
+        <h1 className="start__title">Play Your Cards Right</h1>
+        <div className="start__images">
+          {this.props.allCards.map((card, i) => {
+            return (
+              i <= 4 && <img key={i} src={card.img} className="start__image" />
+            );
+          })}
+        </div>
         <h3>How to play</h3>
-        <ol className='start__instructions'>
+        <ol className="start__instructions">
           <li>You get dealt 3 cards</li>
           <li>You’ll be shown your top card</li>
-          <li>Choose if you think it’s higher, lower or the same as the computer’s card (which you can’t see)</li>
-          <li>See if you guessed right... did you win?</li>
-          <li>You can check the computer's card by scrolling down</li>
-          <li>If you win you get both cards and they go to the bottom of your cards</li>
-          <li>If you lose the computer gets both cards</li>
-          <li>You win the game when you have all the cards</li>
+          <li>
+            Do you think your card’s higher, lower or the same as the
+            computer’s?
+          </li>
+          <li>Were you right?</li>
+          <li>Scroll down to see the computer's card</li>
+          <li>If you win, you get both cards</li>
+          <li>If you lose, the computer gets both cards</li>
+          <li>The game ends when you or the computer has all the cards</li>
         </ol>
 
-        {
-          this.props.ready &&
-          <button className='btn btn__start' onClick={e => this.props.startGame(this.props.allCards, this.props.numCards)}>Start</button>
-        }
-      </div >
-
+        {this.props.ready && (
+          <button
+            className="btn btn__start"
+            onClick={e =>
+              this.props.startGame(this.props.allCards, this.props.numCards)
+            }
+          >
+            Start
+          </button>
+        )}
+      </div>
     );
   }
 }
